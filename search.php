3a12d713fbefc3644e029b28a58eb63a26dc28cc
TanShets <shettytanish02@gmail.com>
2020-11-09 21:13:03 +0530

    Bootstrap link for all

--- search.php
+++ search.php
@@ -2,6 +2,12 @@
 <html>
 <head>
 	<title>Search for Donor</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 	<style type = "text/css">
 		.pop-window{
 			position: absolute;
@@ -61,7 +67,7 @@
 						$_SESSION['message'] = "Request no. $rid completed successfully!!!!";
 						unset($_SESSION['final_solve']);
 						unset($_SESSION['search']);
-						header("Location: emp-login.php");
+						header("Location: employee/emp-requests.php");
 						exit();
 					}
 					else{
