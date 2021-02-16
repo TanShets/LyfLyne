6c531a7bd9cb921083ab8448a38532898f40b912
TanShets <shettytanish02@gmail.com>
2021-02-17 04:02:10 +0530

    Added email sending too

---
+++ email_test.php
@@ -0,0 +1,31 @@
+<?php
+    require_once('PHPMailer/PHPMailerAutoload.php');
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = 'ssl';
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = '465';
+        $mail->isHTML();
+        $mail->Username = 'shetstan@gmail.com';
+        $mail->Password = 'DarkGod@2000';
+        $mail->SetFrom('no-reply@lyflyne.org');
+        $mail->Subject = 'Yo';
+        $mail->Body = 'Hello there';
+        $mail->AddAddress('shettytanish02@gmail.com');
+
+        $mail->Send();
+    }
+?>
+
+<html>
+    <head>
+        <title>Email test</title>
+    </head>
+    <body>
+        <form action = "email_test.php" method = "post">
+            <button type = "submit">Test</button>
+        </form>
+    </body>
+</html>
