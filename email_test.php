277a3a532f3715e35ad1dcf0807aa59c6a731c10
TanShets <shettytanish02@gmail.com>
2021-03-03 00:32:39 +0530

    Continued encryption

--- email_test.php
+++ email_test.php
@@ -9,7 +9,7 @@
         $mail->Host = 'smtp.gmail.com';
         $mail->Port = '465';
         $mail->isHTML();
-        $mail->Username = "shetstan@gmail.com";
+        $mail->Username = decode_level_7('~Lma;SNW~HCLAbV0~_S-E@I>~nb<TOXM~NWLma;S~b<TOXMn~O)A<E:[~RGh\6NI?Y~=rE;y~@aU/GBK~Fg[5MHQ~A<E:[O)~cW1IDMB~fZ4LGPE?+`3)gGl~Q+C>G<]~]7OJSHi~QFg[5MH');
         $mail->Password = decode_level_7("^KX5-:Z3~A<E:[O)~l`:RMVK~eY3KFOD^]6N[80=~]7OJSHi~R,D?H=^?rE;yY~=?Kp/d7-k?5+iIn-b?5+iIn-b?n-b5+iI");
         $mail->SetFrom('no-reply@lyflyne.org');
         $mail->Subject = 'Yo';
