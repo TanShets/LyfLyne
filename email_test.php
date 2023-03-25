d93ee1a8bb48e148f7a845d6352d718b730938bb
TanShets <60138962+TanShets@users.noreply.github.com>
2023-03-26 00:53:48 +0530

    Update email_test.php

--- email_test.php
+++ email_test.php
@@ -30,8 +30,8 @@
         $mail->Host = 'smtp.gmail.com';
         $mail->Port = '465';
         $mail->isHTML();
-        $mail->Username = decode_level_7('~Lma;SNW~HCLAbV0~_S-E@I>~nb<TOXM~NWLma;S~b<TOXMn~O)A<E:[~RGh\6NI?Y~=rE;y~@aU/GBK~Fg[5MHQ~A<E:[O)~cW1IDMB~fZ4LGPE?+`3)gGl~Q+C>G<]~]7OJSHi~QFg[5MH');
-        $mail->Password = decode_level_7("^KX5-:Z3~A<E:[O)~l`:RMVK~eY3KFOD^]6N[80=~]7OJSHi~R,D?H=^?rE;yY~=?Kp/d7-k?5+iIn-b?5+iIn-b?n-b5+iI");
+        $mail->Username = decode_level_7('<Insert encoded email>');
+        $mail->Password = decode_level_7("<Insert encoded password>");
         $mail->SetFrom('no-reply@lyflyne.org');
         $mail->Subject = 'Your One Time Password';
         $mail->Body = "Hello $username!!!! Your One Time Password for account creation is $otp.";
